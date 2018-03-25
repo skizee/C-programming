1bfd00ebf476c68ab9e0177fb73586e80b80f2ae
skizee <9681910+skizee@users.noreply.github.com>
2018-03-25 15:07:34 +0300

    Rename Assign.cpp to Pointers.cpp

---
+++ Pointers.cpp
@@ -0,0 +1,32 @@
+﻿// Assign.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#pragma warning(disable : 4996)
+#include <string.h>
+using namespace std;
+
+
+
+int main()
+{
+	int n = 10;
+	int* pn = &n;
+
+
+	cout << " n=  " << n << " *pn = " << *pn << endl;
+
+	cout << " &n= " << &n << " pn = " << pn << endl;
+
+	int m = *pn;
+
+	*pn = 20;
+
+	cout << " n= " << n << endl;
+
+	return 0;
+}
+
