453a168453d0e368208a02df5eb0583f3bc7467f
skizee <9681910+skizee@users.noreply.github.com>
2018-04-08 18:42:02 +0300

    Rename Reverse.cpp to reverse_number_function.cpp

---
+++ reverse_number_function.cpp
@@ -0,0 +1,32 @@
+﻿#include"stdafx.h"
+#include <iostream>
+using namespace std;
+
+void reverse_number(int number)
+{
+	int reverse = 0;
+	int temp;
+
+	while (number != 0)
+	{
+		temp = number % 10;
+		reverse = reverse * 10 + temp;
+		number = number / 10;
+		
+		if (number == 0)
+		{
+			continue;
+		}
+
+	}
+	cout << reverse<<endl;
+}
+
+int main() {
+
+	int n;
+	cin >> n;
+	reverse_number(n);
+
+	return 0;
+}
