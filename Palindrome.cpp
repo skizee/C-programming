0dde2660466d74612a8535479125cf94bce42bf0
skizee <9681910+skizee@users.noreply.github.com>
2018-04-13 12:25:43 +0300

    Rename Reverse.cpp to Palindrome.cpp

---
+++ Palindrome.cpp
@@ -0,0 +1,47 @@
+﻿
+#include "stdafx.h"
+#include <math.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+void reverse(char arr[10], int size)
+{
+	char temp[10];
+
+
+	for (int i = size - 1; i >= 0; i--)
+	{
+		temp[size - i - 1] = arr[i];
+	}
+
+	temp[size] = '\0';
+
+	if (!strcmp(arr, temp))
+	{
+		cout << "palindrome" << endl;
+	}
+
+	else
+	{
+		cout << "not plaindrome" << endl;
+	}
+
+	cout << temp<<endl;
+}
+
+int main()
+{
+
+	char array[20];
+	cout << "array= ";
+	cin >> array;
+
+	int n = strlen(array);
+
+	reverse(array, n);
+	return 0;
+}
+
+
