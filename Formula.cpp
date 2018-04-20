267baa68aeaa9e0750a424853a2a37a193f841b7
skizee <9681910+skizee@users.noreply.github.com>
2018-04-20 10:57:19 +0300

    Rename ConsoleApplication5.cpp to Formula.cpp
    Нека a е неотрицателно реално число. Да се напише програма, която приближено пресмята квадратен корен от a по метода на Нютон. Упътване: (метод на Нютон) Дефинира се редица от реални числа x0, x1, x2, x3, … по следния начин: 
     
     ... 2, 1, 0,  i ),
    x a
      (x 2 1  x 1  x
    i
    i1i
    0
    =+=
    =
    +
     61 
    Сумирането продължава докато абсолютната стойност на разликата на последните два констуирани елемента на редицата стане по-малка от ε, ε>0, е дадено достатъчно малко реално число.

---
+++ Formula.cpp
@@ -0,0 +1,40 @@
+﻿// ConsoleApplication5.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <math.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+int main()
+{
+	int number;
+
+	cout << "Number:";
+	cin >> number;
+	double eps;
+	cin >> eps;
+	
+	double x0;
+	double x1 = 1;
+
+	do
+	{
+		x0 = x1;
+		x1 = 0.5*(x0 + number / x0);
+		cout << x1 << endl;
+
+	} while (fabs(x1-x0)>=eps);
+	
+	
+
+
+
+	return 0;
+}
