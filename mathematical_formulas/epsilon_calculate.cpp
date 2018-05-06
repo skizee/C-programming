84039cacf4c42c181f8b420b790b3df75340988c
skizee <9681910+skizee@users.noreply.github.com>
2018-05-06 17:41:19 +0300

    Rename epsilon_calculate.cpp to mathematical_formulas/epsilon_calculate.cpp

---
+++ mathematical_formulas/epsilon_calculate.cpp
@@ -0,0 +1,37 @@
+﻿// ConsoleApplication5.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <math.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	cout << "number";
+	double number; cin >> number;
+	
+	cout << "epsilon:";
+	double epsilon; cin >> epsilon;
+
+
+
+	double sum = 1;
+	double total = 1;
+	double temp = 1;
+	int i = 1;
+
+
+	while (fabs(sum)>=epsilon)
+	{
+		sum = sum * (number / i);
+		total = total + sum;
+		i++;
+		cout << total << endl;
+
+	}
+	return 0;
+}
