277ce8fb8b1fecb1496749dd58a74b501e5845cc
skizee <9681910+skizee@users.noreply.github.com>
2018-05-06 17:37:49 +0300

    Rename Reverse_array.cpp to arrays/Reverse_array.cpp

---
+++ arrays/Reverse_array.cpp
@@ -0,0 +1,29 @@
+﻿#include"stdafx.h"
+#include <iostream>
+using namespace std;
+
+void reverse_array(int *arr, int SIZE)
+{
+	for (int i = SIZE - 1; i >= 0; i--)
+	{
+		cout << " " <<arr[i];
+	}
+	cout << endl;
+}
+
+int main() {
+
+	int SIZE;
+	cin >> SIZE;
+
+	int *arr = new int[SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << "Enter array element ";
+		cin >> arr[i];
+	}
+	reverse_array(arr, SIZE);
+
+
+	return 0;
+}
